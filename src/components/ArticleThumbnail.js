cef0b20184e27c490276ed07a1c259c6503349bd
Claire Powers <clairepo@usc.edu>
2019-04-17 21:02:01 -0700

    modularizing comments popup

--- src/components/ArticleThumbnail.js
+++ src/components/ArticleThumbnail.js
@@ -3,7 +3,7 @@ import stock from '../img/stock.jpg'
 import '../css/ArticleThumbnail.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCommentAlt, faArrowCircleUp, faArrowCircleDown, faPaperPlane} from '@fortawesome/free-solid-svg-icons'
-import CommentsList from './CommentsList'
+import CommentsPopup from './CommentsPopup'
 
 class Dash extends Component
 {
@@ -33,52 +33,7 @@ class Dash extends Component
           </div>
         </div>
 
-        <div class="modal fade" id="commentsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"><FontAwesomeIcon icon={faCommentAlt}/></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body" >
-                <ul class="nav nav-tabs" role="tablist">
-                  <li class="nav-item">
-                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Comments</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#menu1" role="tab" aria-controls="menu1" aria-selected="false">Add a Comment</a>
-                  </li>
-                </ul>
-                <br/><br/>
-                <div class="tab-content">
-                  <div id="home" class="tab-pane active">
-                    <CommentsList />
-                  </div>
-                  <div id="menu1" class="tab-pane fade">
-                    <form action="#" method="post" class="form-horizontal" id="commentForm" role="form">
-                        <div class="form-group">
-                            <label for="email" class="col-sm-3 control-label">Comment</label>
-                            <div class="col-sm-10">
-                              <textarea class="form-control" name="addComment" id="addComment" rows="5"></textarea>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-10">
-                                <button class="btn btn-success btn-circle text-uppercase" type="submit" id="submitComment"><span class="glyphicon glyphicon-send"></span> Submit comment</button>
-                            </div>
-                        </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CommentsPopup />
 
         <div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog" role="document">
