955837d8a8e32eeaa87fc32cb0a9ea5c210149ff
Claire Powers <clairepo@usc.edu>
2019-04-19 12:37:34 -0700

    restyling profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Component } from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserCircle, faHistory, faUserFriends } from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle, faHistory, faListUl } from '@fortawesome/free-solid-svg-icons'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TitleNav from './TitleNav'
 import LoginPopup from './LoginPopup'
@@ -32,37 +32,37 @@ class Profile extends Component{
         <div class="container-fluid fixed-top" id="navcontainer">
           <TitleNav />
         </div>
-        <div id="info">
-          <center>
-            <br /><br /><br /><br /><br />
-            <FontAwesomeIcon icon={faUserCircle} size="8x"/><br />
-            <h1 class="display-3"><strong>Claire Powers</strong></h1>
-            <h5>@username</h5><br /><br />
-            <div class="container">
-              <div class="row justify-content-md-center">
+        <br/><br/><br/><br/>
+        <div class="container">
+          <div class="row">
+            <div class="col-2">
+            </div>
+            <div class="col-8">
+              <div class="card bg-light">
+                <div class="card-body">
+                  <center>
+                    <FontAwesomeIcon icon={faUserCircle} size="8x"/><br />
+                    <h1 class="display-3"><strong>Claire Powers</strong></h1>
+                    <h5>@username</h5><br /><br />
+                    <div class="container">
+                      <button type="button" class="btn btn-outline-dark btn-lg" data-toggle="modal" data-target="#topicsmodal"><FontAwesomeIcon icon={faListUl} /> Topics</button>  &nbsp;
+                      <button type="button" class="btn btn-outline-dark btn-lg" data-toggle="modal" data-target="#modal2"> <FontAwesomeIcon icon={faHistory} /> History</button>
+                    </div>
 
-                <div class="col-1">
-                  <a href="/" id="friends" data-toggle="modal" data-target="#topicsmodal">
-                    <FontAwesomeIcon icon={faUserFriends}/>
-                    <h5> Topics </h5>
-                  </a>
-                </div>
-                <div class="col-1">
-                  <a href="/" id="friends"data-toggle="modal" data-target="#modal2">
-                    <FontAwesomeIcon icon={faHistory} />
-                    <h5>History </h5>
-                  </a>
+                  </center>
+
                 </div>
               </div>
             </div>
-
-          </center>
+            <div class="col-2">
+            </div>
+          </div>
         </div>
-      <TopicsPopup />
-      <HistoryPopup />
-      <LoginPopup />
-      <RegisterPopup />
-    </div>
+        <TopicsPopup />
+        <HistoryPopup />
+        <LoginPopup />
+        <RegisterPopup />
+      </div>
     )
   }
 }
