e56beb1bd2e52b8a1e2c4571a7fe153e91a03061
Claire Powers <clairepo@usc.edu>
2019-04-21 19:30:17 -0700

    default checked

--- frontend/src/components/TopicsPopup.js
+++ frontend/src/components/TopicsPopup.js
@@ -6,10 +6,58 @@ class TopicsPopup extends Component{
   constructor(props){
     super(props)
     this.state={
-      loggedIn: this.props.loggedIn
+      loggedIn: this.props.loggedIn,
+      checked1: true,
+      checked2: true,
+      checked3: true,
+      checked4: true,
+      checked5: true,
+      checked6: true,
     }
     this.saveTopics = this.saveTopics.bind(this);
   }
+  toggleCheck1 = (e) => {
+    if(this.state.checked1){
+      this.setState({checked1: false})
+    }else{
+      this.setState({checked1: true})
+    }
+  };
+  toggleCheck2 = (e) => {
+    if(this.state.checked2){
+      this.setState({checked2: false})
+    }else{
+      this.setState({checked2: true})
+    }
+  };
+  toggleCheck3 = (e) => {
+    if(this.state.checked3){
+      this.setState({checked3: false})
+    }else{
+      this.setState({checked3: true})
+    }
+  };
+  toggleCheck4 = (e) => {
+    if(this.state.checked4){
+      this.setState({checked4: false})
+    }else{
+      this.setState({checked4: true})
+    }
+  };
+  toggleCheck5 = (e) => {
+    if(this.state.checked5){
+      this.setState({checked5: false})
+    }else{
+      this.setState({checked5: true})
+    }
+  };
+  toggleCheck6 = (e) => {
+    if(this.state.checked6){
+      this.setState({checked6: false})
+    }else{
+      this.setState({checked6: true})
+    }
+  };
   saveTopics = (e) => {
     //onclick for save changes button
     alert('saving topics')
@@ -28,30 +76,28 @@ class TopicsPopup extends Component{
             <div class="modal-body">
 
             <div class="funkyradio">
-            <input type="checkbox" id="radio0" checked/>
-            <label for="radio1">Test</label>
               <div class="funkyradio-other">
-                  <input type="checkbox" name="radio" id="radio1"/>
+                  <input type="checkbox" name="radio" id="radio1" onClick={this.toggleCheck1} checked={this.state.checked1} />
                   <label for="radio1">Entertainment</label>
               </div>
               <div class="funkyradio-primary">
-                  <input type="checkbox" name="radio" id="radio2"/>
+                  <input type="checkbox" name="radio" id="radio2" onClick={this.toggleCheck2} checked={this.state.checked2}/>
                   <label for="radio2">Sports</label>
               </div>
               <div class="funkyradio-success">
-                  <input type="checkbox" name="radio" id="radio3"  enabled="enabled" checked/>
+                  <input type="checkbox" name="radio" id="radio3" onClick={this.toggleCheck3} checked={this.state.checked3}/>
                   <label for="radio3">Politics</label>
               </div>
               <div class="funkyradio-danger">
-                  <input type="checkbox" name="radio" id="radio4" checked/>
+                  <input type="checkbox" name="radio" id="radio4" onClick={this.toggleCheck4} checked={this.state.checked4}/>
                   <label for="radio4">Technology</label>
               </div>
               <div class="funkyradio-warning">
-                  <input type="checkbox" name="radio" id="radio5" checked/>
+                  <input type="checkbox" name="radio" id="radio5" onClick={this.toggleCheck5} checked={this.state.checked5}/>
                   <label for="radio5">Business</label>
               </div>
               <div class="funkyradio-info">
-                  <input type="checkbox" name="radio" id="radio6" checked/>
+                  <input type="checkbox" name="radio" id="radio6" onClick={this.toggleCheck6} checked={this.state.checked6}/>
                   <label for="radio6">Science</label>
               </div>
           </div>
